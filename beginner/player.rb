b02636236a318fcde70d9ea46aec2d3b5a02a633
Lukas Stejskal <lucastej@gmail.com>
2012-08-03 13:22:40 +0200

    beginner level 4

--- beginner/player.rb
+++ beginner/player.rb
@@ -1,40 +1,65 @@
 
 class Player
 
-  GOOD_HEALTH = 20
+  MAX_HEALTH = 20
 
   BAD_HEALTH = 12
+
+  @@health = MAX_HEALTH
+
+  # REFACTOR to player instance 
 
   def play_turn(warrior)
     display_info(warrior)
 
     if seriously_injured?(warrior)
-      if enemies_nearby?(warrior)
-        retreat!(warrior)
+      if taking_damage?(warrior)
+        attack_or_advance!(warrior)
+      elsif enemies_nearby?(warrior)
+        warrior.retreat!
       else
         warrior.rest!
       end
-    elsif warrior.feel.empty?
-      if injured?(warrior)
-        warrior.rest!
-      else
-        warrior.walk!
-      end
+    elsif injured?(warrior) and not taking_damage?(warrior)
+      warrior.rest!
     else
-      warrior.attack!
+      attack_or_advance!(warrior)
     end
+
+    @@health += health_change(warrior)
   end
 
   def display_info(warrior)
-    puts "Health: %s" % warrior.health
+    puts "Health: %s (%s)" % [ warrior.health, health_change(warrior, "display_as_string") ]
+  end
+
+  def attack_or_advance!(warrior)
+    warrior.feel.empty? ? warrior.walk! : warrior.attack!
   end
 
   def injured?(warrior)
-    warrior.health < GOOD_HEALTH
+    warrior.health < MAX_HEALTH
   end
 
   def seriously_injured?(warrior)
     warrior.health <= BAD_HEALTH
+  end
+
+  # shows how health changed between turns
+  # as_string - add "+" prefix to values > 0
+  #
+  def health_change(warrior, as_string = false)
+    result = warrior.health - @@health
+
+    if as_string
+      "%s%s" % [ ("+" if result > 0), result ]
+    else
+      result
+    end
+  end
+
+  def taking_damage?(warrior)
+    health_change(warrior) < 0
   end
 
   def retreat!(warrior)
