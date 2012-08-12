33fb6a46764261d4a18ec502558ab849dfdb3885
Lukas Stejskal <lucastej@gmail.com>
2012-08-12 22:35:34 +0200

    beginner level 8

--- beginner/player.rb
+++ beginner/player.rb
@@ -9,6 +9,8 @@ class Player
 
   @@direction = nil
 
+  @@ahead = []
+
   # REFACTOR to player instance 
 
   def play_turn(warrior)
@@ -16,22 +18,23 @@ class Player
 
     @@direction ||= :forward
 
+    @@ahead = warrior.look(@@direction)
+
     if warrior.feel(@@direction).wall?
       warrior.pivot!
-    elsif captives_nearby?(warrior)
-      warrior.rescue!(@@direction)
+
+    elsif captives_ahead?(warrior)
+      rescue_or_advance!(warrior)
+
     elsif seriously_injured?(warrior)
-      if taking_damage?(warrior)
-        retreat!(warrior) # attack_or_advance!(warrior)
-      elsif enemies_nearby?(warrior)
-        retreat!(warrior)
-      else
-        warrior.rest!
-      end
+      taking_damage?(warrior) ? retreat!(warrior) : warrior.rest!
+
     elsif injured?(warrior) and not taking_damage?(warrior)
       warrior.rest!
+
     else
       attack_or_advance!(warrior)
+
     end
 
     @@health += health_change(warrior)
@@ -41,8 +44,18 @@ class Player
     puts "Health: %s (%s)" % [ warrior.health, health_change(warrior, "display_as_string") ]
   end
 
+  def rescue_or_advance!(warrior)
+    warrior.feel(@@direction).empty? ? warrior.walk!(@@direction) : warrior.rescue!(@@direction)
+  end
+
   def attack_or_advance!(warrior)
-    warrior.feel(@@direction).empty? ? warrior.walk!(@@direction) : warrior.attack!(@@direction)
+    if enemies_ahead?(warrior)
+      warrior.shoot!(@@direction)
+    elsif enemies_nearby?(warrior)
+      warrior.attack!(@@direction)
+    else
+      warrior.walk!(@@direction)
+    end
   end
 
   def injured?(warrior)
@@ -74,12 +87,29 @@ class Player
     warrior.walk!(@@direction == :forward ? :backward : :forward)
   end
 
+  def nearest_occupied_space()
+    @@ahead.each do |space| 
+      return space if (! space.empty?) && (! space.wall?)
+    end
+
+    nil
+  end
+
+  # is there enemy up to 3 spaces ahead of warrior?
+  def enemies_ahead?(warrior)
+    nearest_occupied_space() && nearest_occupied_space().enemy?
+  end
+
+  # is there an enemy in front of warrior?
   def enemies_nearby?(warrior)
     warrior.feel(@@direction).enemy?
   end
 
-  def captives_nearby?(warrior)
-    warrior.feel(@@direction).captive?
+  def captives_ahead?(warrior)
+    nearest_occupied_space() && nearest_occupied_space().captive?
   end
 
+
 end
+
+# TODO should write tests, eventually (for epic mode)
