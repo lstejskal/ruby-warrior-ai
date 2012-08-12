d6259dc89838d24428406c93e1ceeba1cd372577
Lukas Stejskal <lucastej@gmail.com>
2012-08-12 21:09:12 +0200

    beginner level 7

--- beginner/player.rb
+++ beginner/player.rb
@@ -7,16 +7,24 @@ class Player
 
   @@health = MAX_HEALTH
 
+  @@direction = nil
+
   # REFACTOR to player instance 
 
   def play_turn(warrior)
     display_info(warrior)
 
-    if seriously_injured?(warrior)
+    @@direction ||= :forward
+
+    if warrior.feel(@@direction).wall?
+      warrior.pivot!
+    elsif captives_nearby?(warrior)
+      warrior.rescue!(@@direction)
+    elsif seriously_injured?(warrior)
       if taking_damage?(warrior)
-        attack_or_advance!(warrior)
+        retreat!(warrior) # attack_or_advance!(warrior)
       elsif enemies_nearby?(warrior)
-        warrior.retreat!
+        retreat!(warrior)
       else
         warrior.rest!
       end
@@ -34,7 +42,7 @@ class Player
   end
 
   def attack_or_advance!(warrior)
-    warrior.feel.empty? ? warrior.walk! : warrior.attack!
+    warrior.feel(@@direction).empty? ? warrior.walk!(@@direction) : warrior.attack!(@@direction)
   end
 
   def injured?(warrior)
@@ -63,11 +71,15 @@ class Player
   end
 
   def retreat!(warrior)
-    warrior.walk!(:backward)
+    warrior.walk!(@@direction == :forward ? :backward : :forward)
   end
 
   def enemies_nearby?(warrior)
-    warrior.feel.enemy?
+    warrior.feel(@@direction).enemy?
+  end
+
+  def captives_nearby?(warrior)
+    warrior.feel(@@direction).captive?
   end
 
 end
