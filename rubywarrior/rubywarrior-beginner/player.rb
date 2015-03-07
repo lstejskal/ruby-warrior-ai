20f8af69e0f1cd3d781282f59a101bbab47dd7bc
Lukas Stejskal <lucastej@gmail.com>
2015-03-07 10:04:29 +0100

    beginner level 4

--- rubywarrior/rubywarrior-beginner/player.rb
+++ rubywarrior/rubywarrior-beginner/player.rb
@@ -4,13 +4,14 @@ class Player
   MAX_HEALTH = 20
 
   def initialize()
+    @health_previous_turn ||= MAX_HEALTH
   end
 
   def play_turn(warrior)
     @warrior = warrior
 
     if @warrior.feel.empty?
-      if @warrior.health < MAX_HEALTH
+      if injured? and not taking_damage?
         @warrior.rest!
       else
         @warrior.walk!
@@ -18,6 +19,20 @@ class Player
     else
       @warrior.attack!
     end
+
+    @health_previous_turn = @warrior.health
+  end
+
+  def injured?
+    @warrior.health < MAX_HEALTH
+  end
+
+  def taking_damage?
+    @warrior.health < @health_previous_turn
+  end
+
+  def retreat!
+    @warrior.walk!(:backward)
   end
 
 end
